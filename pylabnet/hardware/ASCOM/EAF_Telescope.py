7082984e7303d7106a7c6adb4c00d2b95ee65d6c
AG Riedinger <quantum.networks.uhh@gmail.com>
2024-02-01 15:57:14 +0100

    New hardware added (Arduino Galvo and EAF Telescope)

---
+++ pylabnet/hardware/ASCOM/EAF_Telescope.py
@@ -0,0 +1,151 @@
+import ctypes
+import time
+from pyvisa import ResourceManager
+from pylabnet.utils.logging.logger import LogHandler
+
+
+class Driver:
+    def __init__(self, lib_path="", device_id=0, logger=None):
+
+        #prepare logging asnd connect logger
+        self.log = LogHandler(logger=logger)
+        self.rm = ResourceManager()
+        self.log.info("EAF-Logger established :)")
+
+        #prepare libary of EAF
+        self.lib_path = lib_path
+        self.device_id = device_id
+        #self.lib = ctypes.cdll.LoadLibrary(lib_path)
+
+        try:
+            self.lib = ctypes.cdll.LoadLibrary(lib_path)
+            self.log.info("Libary geladen")
+        except:
+            self.log.info("Error while loading Libary. Wrong Path? Used path: " + lib_path)
+
+        #load ID-List of ASCOM devices. Without this the EAF ID won't be found
+        self.log.info("Number of Ascom devices: " + str(self.EAFGetNum()))
+        #open the device
+        #print(self.EAFGetNum())
+        self.EAFopen()
+        try:
+            self.EAFmove(10000)
+            time.sleep(1)
+            self.log.info(self.EAFis_moving())
+            self.EAFstop()
+            self.log.info(self.EAFis_moving())
+
+        except:
+            self.log.info("Error while  moving")
+
+    def EAFGetNum(self):
+        """ Descriptions:
+        This should be the first API to be called
+        get number of connected EAF focuser, call this API to refresh device list if EAF is connected
+        or disconnected
+
+        Return: number of connected EAF focuser. 1 means 1 focuser is connected.
+        """
+
+        num_devices = self.lib.EAFGetNum()
+        return num_devices
+
+    def EAFopen(self):
+        self.log.info("Try to open EAF")
+        back = self.lib.EAFOpen(int(self.device_id))
+
+        if back != 0:
+            self.log.info("An error occured while opening the EAF device. Error code: " + str(back))
+
+    def EAFclose(self):
+        return self.lib.EAFClose(self.device_id)
+
+    def EAFCheck(self, pid):
+        #VID is 0x03C3 for EAF
+        # Check if an EAF device with the specified Vendor ID (vid) and Product ID (pid) is connected
+        is_connected = self.lib.EAFCheck(0x03C3, ctypes.c_uint16(pid))
+        return is_connected
+
+    def EAFGetID(self, index):
+        # Get the ID of the EAF device at the specified index
+        PointerInt = ctypes.c_int()
+        id = self.lib.EAFGetID(int(index), ctypes.byref(PointerInt))
+        return PointerInt.value, id
+
+    def EAFget_position(self):
+        position = ctypes.c_int()
+        self.lib.EAFGetPosition(int(self.device_id), ctypes.byref(position))
+        return position.value
+
+    def EAFGetSDKVersion(self):
+        # Get the SDK version of the EAF library
+        sdk_version = self.lib.EAFGetSDKVersion()
+        return sdk_version
+
+    def EAFmove(self, steps):
+        return self.lib.EAFMove(int(self.device_id), int(steps))
+
+    def EAFstop(self):
+        self.lib.EAFStop(int(self.device_id))
+
+    def EAFis_moving(self):
+        is_moving = ctypes.c_bool()
+        is_hand_control = ctypes.c_bool()
+        self.lib.EAFIsMoving(int(self.device_id), ctypes.byref(is_moving), ctypes.byref(is_hand_control))
+        return is_moving.value
+
+    def EAFResetPosition(self, steps):
+        # Reset the position of the EAF device with the specified ID to the specified number of steps
+        return self.lib.EAFResetPostion(int(self.device_id), int(steps))
+
+    def EAFgetTemp(self):
+        temperature = ctypes.c_float()
+        self.lib.EAFGetTemp(self.device_id, ctypes.byref(temperature))
+        return temperature.value
+
+    def EAFset_beep(self, beep):
+        # Enable or disable the beep sound for the EAF device with the specified ID
+        self.lib.EAFSetBeep(self.device_id, beep)
+
+    def EAFget_beep(self):
+        # Check if the beep sound is enabled for the EAF device with the specified ID
+        is_beep = ctypes.c_bool()
+        self.lib.EAFGetBeep(self.device_id, ctypes.byref(is_beep))
+        return is_beep.value
+
+    def EAFget_backlash(self):
+        # Get the backlash of the EAF device with the specified ID
+        backlash = ctypes.c_int()
+        self.lib.EAFGetBacklash(self.device_id, ctypes.byref(backlash))
+        return backlash.value
+
+    def EAFset_backlash(self, backlash_value):
+        #Set the backlash of the EAF device with the specified ID
+        self.lib.EAFSetBacklash(self.device_id, backlash_value)
+
+    def EAFget_reverse(self):
+        # Get the reverse status of the EAF device with the specified ID
+        is_reverse = ctypes.c_bool()
+        self.lib.EAFGetReverse(self.device_id, ctypes.byref(is_reverse))
+        return is_reverse.value
+
+    def EAFset_reverse(self, reverse_status):
+        # Set the reverse status of the EAF device with the specified ID
+        self.lib.EAFSetReverse(self.device_id, reverse_status)
+
+    def EAFstep_range(self):
+        # Get the step range of the EAF device with the specified ID
+        step_range = ctypes.c_int()
+        self.lib.EAFStepRange(self.device_id, ctypes.byref(step_range))
+        return step_range.value
+
+    def EAFget_max_step(self):
+        # Get the max range of the EAF device with the specified ID
+
+        max_range = ctypes.c_int()
+        error_code = self.lib.EAFGetMaxStep(self.device_id, ctypes.byref(max_range))
+        return max_range.value
+
+    def EAFSetMaxStep(self, max_step):
+        # Set the maximum step value of the EAF device with the specified ID
+        error_code = self.lib.EAFSetMaxStep(self.device_id, max_step)
--- pylabnet/hardware/arduino/arduino_galvo.py
+++ pylabnet/hardware/arduino/arduino_galvo.py
@@ -1,20 +1,17 @@
-import numpy as np
-from pyvisa import VisaIOError, ResourceManager
+#important imports
+from pyvisa import ResourceManager
 from pylabnet.utils.logging.logger import LogHandler
-import serial
+import serial #for connection to arduino via usb
 import cv2
 
 
-print('hallo ich bin der driver')
-
 class Driver:
-    def __init__(self, minVoltage, maxVoltage, distanceToPicture, port = 'COM3', baudrate = 115200, bitsResolution = 10, logger=None):  # noqa: E501
-        
-        #initiate log:
-        
+    def __init__(self, minVoltage, maxVoltage, distanceToPicture, port='COM3', baudrate=115200, bitsResolution=10, logger=None):  # noqa: E501
+
+        #initiate log: For logging in pylabnet
         self.log = LogHandler(logger=logger)
         self.rm = ResourceManager()
-        self.log.info("1YAY :)")
+        self.log.info("Arduino-Logger established :)")
 
         #arduino code
         self.port = port
@@ -22,107 +19,88 @@ class Driver:
 
         #Connect to Arduino
         try:
-            self.log.info("YAY :)")
-            self.arduino = serial.Serial(self.port,  baudrate=115200, timeout=.1)
+            self.arduino = serial.Serial(self.port, baudrate=115200, timeout=.1)
             self.log.info(f"Successfully connected to Arduino.")
         except:
-            print("Error while Connecting. Wrong Port? Used port: "+self.port)
+            self.log.info("Error while Connecting. Wrong Port? Used port: " + self.port)
 
-
-
+        #This is atm not impoortant and does not work
         self.minVoltage = minVoltage
         self.maxVoltage = minVoltage
         self.MirrorMaxVoltage = -5
         self.MirrorMaxVoltage = 5
-        self.bitResolution = bitsResolution
         self.distance = distanceToPicture
-        self.xCenter = 0
-        self.yCenter = 0
-        
+
+        #actual important variables
+        self.bitResolution = bitsResolution
+
         #maybe safe this in file an load.
         self.xCenter = 0
         self.yCenter = 0
 
         if self.MirrorMaxVoltage < self.minVoltage:
-            print("Careful. Maximum applyable voltage is "+self.maxVoltage+" Volts. Spectra of Mirror is -5 to 5 Volts for 60°")
+            self.log.info("Careful. Maximum applyable voltage is " + self.maxVoltage + " Volts. Spectra of Mirror is -5 to 5 Volts for 60°")
 
-        self.maxUsageAngle = 60*self.maxVoltage/self.MirrorMaxVoltage
+        self.maxUsageAngle = 60 * self.maxVoltage / self.MirrorMaxVoltage
         self.minimumAngle = self.maxUsageAngle / 2**(self.bitResolution)
 
+        #might be a code idea in future. Maybe just set them to xCenter and yCenter
         #setDefaultCoords(500,400)
-        #time.sleep(0.2)
-        #send_area_scan_0to1024(700,700,times=100) #good is 350, 300 for scanning area
-        
-        #time.sleep(0.8)
 
-
+    #This functions sets the Galvo on the centercoords
 
     def setDefaultCoords(self, x_default, y_default):
         self.xCenter = x_default
         self.yCenter = y_default
-        self.arduino.write(bytes("center"+";"+str(x_default)+";"+str(y_default),  'utf-8'))
+        self.arduino.write(bytes("center" + ";" + str(x_default) + ";" + str(y_default), 'utf-8'))
+        #adjust in future such that Arduino sends back a message if it is done
 
-    #Length Informations making no sense right now. Maybe later
-    # def applyVoltageToDAC(self, DAC_pin, movingLength):
-    
-    #     amountOfSteps = int(round(np.arcsin(movingLength/self.distance)/(self.minimumAngle/360*2*np.pi)))  # noqa: E501
-    #     self.arduino.write(bytes(DAC_pin+";"+str(amountOfSteps),  'utf-8'))
+    #Sets the voltage of the DAC0 or DAC1 to a specific value. Therfore set x and cords to specific value
+    def applyVoltageToDAC0to1024(self, DAC_pin, steps):
 
-    #     data = self.arduino.readline()
-
-    
-    def applyVoltageToDAC0to1024(self, DAC_pin, steps ):
-    
         amountOfSteps = steps # noqa: E501
         try:
-            self.arduino.write(bytes(DAC_pin+";"+str(amountOfSteps),  'utf-8'))
+            self.arduino.write(bytes(DAC_pin + ";" + str(amountOfSteps), 'utf-8'))
             data = self.arduino.readline()
             return data
-        
+
         except:
-            print("Error occured while trying to write to the Arduino. Is he connected, is the rigth port used?")
+            self.log.critical("Error occured while trying to write to the Arduino. Is he connected, is the rigth port used?")
             return "Error"
-        
-    #Length Information are useless right now. Maybe later
-    # def send_area_scan_Length(self, x_length, y_hight):
-    #     messageToSend = "area"+";"+str(x_length)+";"+str(y_hight)+";"+str(self.distance)
-    #     self.arduino.write(bytes("area"+";"+str(x_length)+";"+str(y_hight)+";"+str(self.distance),  'utf-8'))
-    #     print("Sending: "+messageToSend)
-    #     time.sleep(0.1)
-    #     data = self.arduino.readline()#
 
         return data
-    
-    def send_area_scan_0to1024(self, x_steps, y_steps, x_center = -1, y_center=-1, times = 100):
+
+    #This sends a command to arduino to scan specific area. It is possible to set the center of the scan
+    #x_steps and y_steps are the amount of steps the galvo should take in x and y direction. Its basiccally the size of the area scanned
+    def send_area_scan_0to1024(self, x_steps, y_steps, x_center=-1, y_center=-1, times=100):
 
         if x_center != -1 or y_center != -1:
-            messageToSend = "areaStep"+";"+str(x_center)+";"+str(y_center)+";"+str(x_steps)+";"+str(y_steps)+";"+str(times)
-            self.arduino.write(bytes("areaStep"+";"+str(x_center)+";"+str(y_center)+";"+str(x_steps)+";"+str(y_steps)+";"+str(times),  'utf-8'))
-            print("Sending: "+messageToSend)
-        
+            messageToSend = "areaStep" + ";" + str(x_center) + ";" + str(y_center) + ";" + str(x_steps) + ";" + str(y_steps) + ";" + str(times)
+            self.arduino.write(bytes("areaStep" + ";" + str(x_center) + ";" + str(y_center) + ";" + str(x_steps) + ";" + str(y_steps) + ";" + str(times), 'utf-8'))
+            #print("Sending: "+messageToSend)
+
         else:
-            messageToSend = "areaStep"+";"+str(self.xCenter)+";"+str(self.yCenter)+";"+str(x_steps)+";"+str(y_steps)+";"+str(times)
-            self.arduino.write(bytes("areaStep"+";"+str(self.xCenter)+";"+str(self.yCenter)+";"+str(x_steps)+";"+str(y_steps)+";"+str(times),  'utf-8'))
-            print("Sending: "+messageToSend)
+            messageToSend = "areaStep" + ";" + str(self.xCenter) + ";" + str(self.yCenter) + ";" + str(x_steps) + ";" + str(y_steps) + ";" + str(times)
+            self.arduino.write(bytes("areaStep" + ";" + str(self.xCenter) + ";" + str(self.yCenter) + ";" + str(x_steps) + ";" + str(y_steps) + ";" + str(times), 'utf-8'))
+            #print("Sending: "+messageToSend)
 
-
-        
-        print("Please wait until arduino is finished")
+        self.log.info("Please wait until arduino is finished")
 
         message = ""
         while message == "" or message == b'':
             message = self.arduino.readline(4)
-        
+
         return message
 
+    #this is just a fun function with which one can draw a picture with the laser
 
-    def makeLaserPicture(self, path = ""):
+    def makeLaserPicture(self, path=""):
 
         #fn = path
         im_gray = cv2.imread('herzz.png', cv2.IMREAD_GRAYSCALE)
-        im_gray = cv2.resize(im_gray,(100,100),interpolation= cv2.INTER_LINEAR)  # noqa: E501
+        im_gray = cv2.resize(im_gray, (100, 100), interpolation=cv2.INTER_LINEAR)  # noqa: E501
 
-        img_blur = cv2.GaussianBlur(im_gray, (3,3), 0)
+        img_blur = cv2.GaussianBlur(im_gray, (3, 3), 0)
         sobelxy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5)
         cv2.imshow('Sobel X Y using Sobel() function', sobelxy)
         #cv2.waitKey(0)
@@ -131,18 +109,18 @@ class Driver:
         cv2.imshow('Canny Edge Detection', edges)
         #cv2.waitKey(0)
 
-        im_gray = edges//255
+        im_gray = edges // 255
         img_list = im_gray.tolist()
         result = ""
         for row in img_list:
             row_str = [str(p) for p in row]
-            result +=  "".join(row_str)
+            result += "".join(row_str)
 
-        result = ""+ result
+        result = "" + result
         # #send to arduino:
-        print(result,  file=open('C:\\Users\\timoe\\OneDrive\\Desktop\\Output.txt', 'w'))
-        
-        self.arduino.write(bytes("pict"+";", 'utf-8'))
+        print(result, file=open('Output.txt', 'w'))
+
+        self.arduino.write(bytes("pict" + ";", 'utf-8'))
         print("go")
 
         #wait for answer
@@ -153,11 +131,9 @@ class Driver:
         return message
         #time.sleep(5)
         #print(self.arduino.readline())
-    
-        #print(result)
-        
 
-            
+        #print(result)
+
     #This function should allow the user to set one specific center for the laser to hit
     #def moveCenter(self):
         # with pynput.keyboard.Listener(on_release=on_key_release) as listener:
